f9b9f474e0d53bc505b18f9e25aa1bab9a28b57e
Kevin Vargas <keving.vargasb@gmail.com>
2021-02-06 17:29:11 -0500

    initial commit

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Image } from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
+const OnboardingScreen = ({ navigation }) => {
+  return (
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: '#fff',
+          image: <Image source={require('../assets/onboarding-img1.png')} />,
+          title: 'Onboarding',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+        {
+          backgroundColor: '#fff',
+          image: <Image source={require('../assets/onboarding-img2.png')} />,
+          title: 'Onboarding 2',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+        {
+          backgroundColor: '#fff',
+          image: <Image source={require('../assets/onboarding-img3.png')} />,
+          title: 'Onboarding 3',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+      ]}
+    />
+  );
+};
+
+export default OnboardingScreen;
